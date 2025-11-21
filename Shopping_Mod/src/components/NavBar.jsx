3bad7085809ccbd5fe8d2e7c72689530ea496e3c
DAbdulRaheem <raheemabduld56@gmail.com>
2025-11-21 12:19:05 +0530

    modifies styling

--- Shopping_Mod/src/components/NavBar.jsx
+++ Shopping_Mod/src/components/NavBar.jsx
@@ -5,13 +5,12 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       {/* Left side links */}
-      <div className="nav-links">
-        <Link to="/" className="nav-item">Home</Link>
-        <Link to="/cart" className="nav-item">Cart 🛒</Link>
-      </div>
 
       {/* Right side links (Auth) */}
+      <h2 style={{color:"white"}}>Shopping_Mod</h2>
       <div className="nav-auth">
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/cart" className="nav-item">Cart 🛒</Link>
         <Link to="/login" className="nav-item login-btn">Login</Link>
         <Link to="/register" className="nav-item register-btn">Register</Link>
       </div>
